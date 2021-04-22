bee0333b14ee09ba91520d7ade91e5312d636af6
alban maillere <alban.prestatairemaillere@lactalis.fr>
2021-04-22 17:04:59 +0200

    [AMAI] maj

--- import-cert-file.sh
+++ import-cert-file.sh
@@ -13,7 +13,9 @@ PWD="changeit"
 declare -a trustStores=("C:\Program Files\AdoptOpenJDK\jdk-11.0.9.101-hotspot\lib\security\cacerts" 
                 "C:\Program Files\Java\java-1.8.0-openjdk-1.8.0.275-1.b01.dev.redhat.windows.x86_64\jre\lib\security\cacerts"
                 "C:\Users\maillerea\AppData\Roaming\JetBrains\IntelliJIdea2020.3\ssl\cacerts"
+                "C:\prog\Projects\lactalis\application-lactaflux-mongo\src\main\docker\cacerts"
                 )
+
 for KS in "${trustStores[@]}"
 do
 	keytool -import -noprompt -alias "$ALIAS" -file "$FILE" -keystore "$KS" -storepass $PWD
